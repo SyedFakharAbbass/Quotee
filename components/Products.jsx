bd9bb87ba340894d223b6747e23719e67ebb5129
Hammad Nadir <Hammad.Nadir@Biotech.com>
2023-01-17 15:42:08 +0500

    abcd

--- components/Products.jsx
+++ components/Products.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setProducts, setStyles } from '../redux/data';
 
 const Products = () => {
 
-    const { products } = useSelector((state) => state.data)
+    const { products } = useSelector((state) => state.data);
+    const [colors, setColors] = useState(true)
+
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -25,6 +27,14 @@ const Products = () => {
         dispatch(setProducts("three"))
         dispatch(setStyles(""))
     }
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setColors(!colors);
+        }, 500);
+        return () => clearInterval(intervalId);
+      }, [colors]);
+    
 
     return (
         <div className='container '>
@@ -66,7 +76,11 @@ const Products = () => {
                         <p className='text-center text-[#008bbf] text-[15px] pb-8 font-medium'>let our team to help you</p>
                         <p className='text-center text-[#008bbf] text-[15px] pb-10 font-medium'>chat with our team to find best quote for your product</p>
                         <div>
-                            <button className='flex m-auto content-center w-[140px] bg-[ #008BBF] bg-[#008BBF] text-white items-center p-3 rounded-[10px]'><img className='pr-2' src="/images/vector.png" />CHAT NOW</button>
+                            {
+                                colors ?
+                                    <button className='flex m-auto content-center w-[175px] h-[60px] bg-[#008BBF] justify-center text-white items-center p-3 rounded-[10px]'><img className='pr-2' src="/images/vector.png" />CHAT NOW</button>
+                                    : <button className='flex m-auto content-center w-[175px] h-[60px] justify-center bg-[#DBF4FF] text-[#008BBF] font-extrabold items-center p-3 rounded-[10px]' style={{boxShadow :  "0px 4px 4px rgba(0, 0, 0, 0.25)"}}><img className='pr-2' src="/images/vector1.png" />CHAT NOW</button>
+                            }
                         </div>
                     </div>
                 </div>
