88f8c647f13a1e0d9a3afce3f0beff6462ba3561
Hammad Nadir <Hammad.Nadir@Biotech.com>
2023-01-20 18:50:41 +0500

    okkk

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -55,7 +55,7 @@ function Modal({ show, setShow, handleModalClick, setError, error }) {
         <div className=''>
             {show && <div><div className="z-5 bg-opacity-50 flex h-[100vh] fixed justify-center items-center top-0 right-0 bottom-0 left-0" onClick={() => setShow(false)} style={{ background: "rgba(12, 11, 11, 0.8)" }}>
             </div>
-                <div className="bg-white min-w-[650px] max-w-[100%] pt-[30px] pb-[25px] px-[60px] rounded-[10px] text-center fixed main_class">
+                <div className="bg-white adjust_modal pt-[30px] pb-[25px] px-[60px] rounded-[10px] text-center fixed main_class">
                     <h1 className="mb-[30px] font-bold text-[26px] text-[#008BBF]">You Are Just One Step Ahead..!</h1>
                     <div><img src="/images/close.png" onClick={() => setShow(false)} className='cursor-pointer absolute top-[10px] right-[10px] h-[30px]'/></div>
                     <div className='flex flex-col mb-[15px]'>
