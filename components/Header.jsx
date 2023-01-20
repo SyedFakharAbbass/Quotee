743c0f6fc3cf407fc7a2baafcf360c70b94a6bd9
SyedFakharAbbass <mailtofakharabbas@gmail.com>
2023-01-20 15:21:32 +0500

    quotee

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
         {/* <p className='text-[#008bbf] pl-[50px] '>Fast, Free and Reliable</p> */}
         <p className='w-[76%] text-[#008BBF]  pt-7'>As we are Print247, we are available 24/7 to assist and guide you. You just have to inform us about the packaging and we will create an instant quote for you. All you have to answer few questions related to size, material, features and numbers. For as many different setups as you might require, create as many quotes as you like. Quotes for printed roll stock or custom-sized bags can be requested and will be made within 2-3 business days.</p>
       </div>
-      <hr className='mt-[44px]' />
+      <hr className='mt-[44px] borr' />
     </div>
   )
 }
